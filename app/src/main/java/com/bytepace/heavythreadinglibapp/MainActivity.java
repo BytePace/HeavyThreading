7e55ecd53d5f438ff80be9c8ab8fdf7a84ac1968
Viktor <viktor.kinko@sy-dev.com>
2018-02-20 15:31:30 +0600

    [upd] made postThread and doneThread run in calling Thread instead of calculation thread. Removed Log calls

--- app/src/main/java/com/bytepace/heavythreadinglibapp/MainActivity.java
+++ app/src/main/java/com/bytepace/heavythreadinglibapp/MainActivity.java
@@ -2,12 +2,12 @@ package com.bytepace.heavythreadinglibapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.bytepace.heavythreading.HeavyThreading;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import rx.Subscriber;
@@ -22,18 +22,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         text_view = findViewById(R.id.text_view);
         text_view.setText("");
-        int max = 200000;
-        singleThreadSimpleCount(max);
-    }
-
-    public static String getThreadSignature() {
-        Thread t = Thread.currentThread();
-        long l = t.getId();
-        String name = t.getName();
-        long p = t.getPriority();
-        String gname = t.getThreadGroup().getName();
-        return (name + ":(id)" + l + ":(priority)" + p
-                + ":(group)" + gname);
+        final int max = 50000;
+        singleThreadSimpleCount(max, new Runnable() {
+            @Override
+            public void run() {
+                multiThreadSimpleCount(max);
+            }
+        });
     }
 
     private boolean checkSimple(int number) {
@@ -47,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    private void singleThreadSimpleCount(final int to_max) {
+    private void singleThreadSimpleCount(final int to_max, final Runnable r) {
         text_view.setText("Started single thread for " + to_max + " units");
         Thread t = new Thread(new Runnable() {
             @Override
@@ -55,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
                 int simpleCount = 0;
                 final long time = System.currentTimeMillis();
                 for (int i = 1; i < to_max; i++) {
-                    if (i % 100000 == 0) Log.d("whoa", i + "");
                     boolean result = checkSimple(i);
                     if (result) {
                         simpleCount++;
@@ -66,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         text_view.setText(text_view.getText() + "\nSINGLE THREAD   count = " + finalSimpleCount + "; time = " + (System.currentTimeMillis() - time));
-                        multiThreadSimpleCount(to_max);
+                        r.run();
                     }
                 });
             }
@@ -74,18 +68,21 @@ public class MainActivity extends AppCompatActivity {
         t.start();
     }
 
-    private void multiThreadSimpleCount(final int to_max) {
+    private void multiThreadSimpleCount(int to_max) {
         text_view.setText(text_view.getText() + "\nMultiple single thread for " + to_max + " units");
         final int[] simpleCount = {0};
-        final long time = System.currentTimeMillis();
-        HeavyThreading.load(null, new HeavyThreading.LoaderInterface<Integer, ArrayList<Integer>, Integer>() {
+        long time = System.currentTimeMillis();
+        Map<String, Long> params = new HashMap<>();
+        params.put("max", (long) to_max);
+        params.put("start_time", time);
+        params.put("division", 8L);
+        HeavyThreading.load(params, new HeavyThreading.LoaderInterface<Long, ArrayList<Integer>, Integer>() {
             @Override
-            public void preThread(Subscriber<? super ArrayList<Integer>> subscriber, Map<String, Integer> incoming_settings) {
+            public void preThread(Subscriber<? super ArrayList<Integer>> subscriber, Map<String, Long> incoming_settings) {
                 ArrayList<Integer> batch = new ArrayList<>();
-                for (int i = 1; i <= to_max; i++) {
-                    if (i % 100000 == 0) Log.d("whoa", i + "");
+                for (int i = 1; i <= incoming_settings.get("max"); i++) {
                     batch.add(i);
-                    if (batch.size() > to_max / 8) {
+                    if (batch.size() > incoming_settings.get("max") / incoming_settings.get("division")) {
                         subscriber.onNext(batch);
                         batch = new ArrayList<>();
                     }
@@ -97,8 +94,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public Integer onThread(ArrayList<Integer> batch, Map<String, Integer> incoming_settings) {
-                Log.d("whoa", batch.size() + "");
+            public Integer onThread(ArrayList<Integer> batch, Map<String, Long> incoming_settings) {
                 int batch_simple_count = 0;
                 for (Integer i : batch) {
                     if (checkSimple(i)) {
@@ -109,18 +105,16 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void postThread(Integer batch_simple_count, Map<String, Integer> incoming_settings) {
+            public void postThread(Integer batch_simple_count, Map<String, Long> incoming_settings) {
                 simpleCount[0] += batch_simple_count;
             }
 
             @Override
-            public void doneThread() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        text_view.setText(text_view.getText() + "\nMULTIPLE THREAD   count = " + simpleCount[0] + "; time = " + (System.currentTimeMillis() - time)+ "\ndone");
-                    }
-                });
+            public void doneThread(Map<String, Long> incoming_settings) {
+                long time = System.currentTimeMillis() - incoming_settings.get("start_time");
+                text_view.setText(text_view.getText() +
+                        "\nMULTIPLE THREAD   count = " + simpleCount[0] + "; time = " + time +
+                        "\ndone");
             }
         });
     }
--- heavythreading/src/main/java/com/bytepace/heavythreading/HeavyThreading.java
+++ heavythreading/src/main/java/com/bytepace/heavythreading/HeavyThreading.java
@@ -1,5 +1,7 @@
 package com.bytepace.heavythreading;
 
+import android.os.Handler;
+
 import java.util.Map;
 
 import rx.Observable;
@@ -18,6 +20,7 @@ public class HeavyThreading {
 
     /**
      * Interface used for callbacks in {@link HeavyThreading#load load} method
+     *
      * @param <U> Incoming data map type
      * @param <V> Internal data type before heavy transformation
      * @param <T> Data type after heavy transformation
@@ -25,8 +28,9 @@ public class HeavyThreading {
     public interface LoaderInterface<U, V, T> {
         /**
          * Called on start of the sequence. Used to emit values, based on incoming_settings.
-         * @param subscriber An RxJava subscriber. Use onNext(V) method to emit data.
-         *                   Use onCompleted() method to signal that all data is emitted.
+         *
+         * @param subscriber        An RxJava subscriber. Use onNext(V) method to emit data.
+         *                          Use onCompleted() method to signal that all data is emitted.
          * @param incoming_settings Map of incoming data to base the generation of data on.
          */
         void preThread(Subscriber<? super V> subscriber, Map<String, U> incoming_settings);
@@ -34,7 +38,8 @@ public class HeavyThreading {
         /**
          * Called on start of heavy loading computation section. This method will be executed in
          * parralel threads, so be careful about deadlocks and race conditions.
-         * @param emitted_data Data emitted by preThread.
+         *
+         * @param emitted_data      Data emitted by preThread.
          * @param incoming_settings Settings that emitted data base on.
          * @return Data after heavy computation
          */
@@ -42,52 +47,62 @@ public class HeavyThreading {
 
         /**
          * Called when heavy computation is done for a next part of the emitted data.
-         * @param computed_data Data, emitted by preThread and computed by onThread.
+         *
+         * @param computed_data     Data, emitted by preThread and computed by onThread.
          * @param incoming_settings Settings that emitted/computed data base on.
          */
         void postThread(T computed_data, Map<String, U> incoming_settings);
 
         /**
          * Called when every piece of data is computed and postThread-processed.
+         *
+         * @param incoming_settings Settings that emitted data base on.
          */
-        void doneThread();
+        void doneThread(Map<String, U> incoming_settings);
     }
 
 
     /**
      * Generic RxJava method that allows to emit some data and do some heavy calculations
      * in parallel calculation threads.
+     *
      * @param incoming_settings Map of parameters to use in threads
-     * @param callback LoaderInterface callback
-     * @param <U> Incoming data map type
-     * @param <V> Internal data type before heavy transformation
-     * @param <T> Data type after heavy transformation
+     * @param callback          LoaderInterface callback
+     * @param <U>               Incoming data map type
+     * @param <V>               Internal data type before heavy transformation
+     * @param <T>               Data type after heavy transformation
      */
     public static <U, V, T> void load(final Map<String, U> incoming_settings, final LoaderInterface<U, V, T> callback) {
+        final Handler handler = new Handler();
         Observable.create(new Observable.OnSubscribe<V>() {
             @Override
             public void call(Subscriber<? super V> subscriber) {
                 callback.preThread(subscriber, incoming_settings);
             }
-        }).flatMap(new Func1<V, Observable<T>>() {
-            @Override
-            public Observable<T> call(V result) {
-                return Observable.just(result)
-                        .subscribeOn(Schedulers.computation())
-                        .map(new Func1<V, T>() {
-                            @Override
-                            public T call(V s) {
-                                return callback.onThread(s, incoming_settings);
-                            }
-                        });
-            }
-        })
-                .observeOn(Schedulers.computation())
-                .subscribeOn(Schedulers.newThread())
+        }).subscribeOn(Schedulers.computation())
+                .flatMap(new Func1<V, Observable<T>>() {
+                    @Override
+                    public Observable<T> call(V result) {
+                        return Observable.just(result)
+                                .subscribeOn(Schedulers.computation())
+                                .map(new Func1<V, T>() {
+                                    @Override
+                                    public T call(V s) {
+                                        return callback.onThread(s, incoming_settings);
+                                    }
+                                });
+                    }
+                })
+                .observeOn(Schedulers.newThread())
                 .subscribe(new Subscriber<T>() {
                     @Override
                     public void onCompleted() {
-                        callback.doneThread();
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                callback.doneThread(incoming_settings);
+                            }
+                        });
                     }
 
                     @Override
@@ -96,8 +111,14 @@ public class HeavyThreading {
                     }
 
                     @Override
-                    public void onNext(T tResult) {
-                        callback.postThread(tResult, incoming_settings);
+                    public void onNext(final T tResult) {
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                callback.postThread(tResult, incoming_settings);
+                            }
+                        });
+
                     }
                 });
     }
